37041f6515e0c57ece11133816ab172a7c4888ea
DeepaBaddur <deepa.baddur@cognizant.com>
2020-06-08 15:34:19 +0530

    Addedc changes in branch1.

--- src/com/gittestrepo/demo/Demo.java
+++ src/com/gittestrepo/demo/Demo.java
@@ -3,6 +3,7 @@ package com.gittestrepo.demo;
 public class Demo {
 	
 	public static void main(String[] args) {
+		System.out.println("This code is there in branch1");
 		System.out.println("Linearsearch algorithm");		
 		// Let's write some code.
 		int arr[] = {2, 3, 4, 10, 18, 17, 40};
